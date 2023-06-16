55cfc42fa4f229c90f38875f77135cb79fc7e7b8
marcelo <marcelomotadev@gmail.com>
2023-06-16 18:20:33 -0300

    project start

---
+++ src/routes/users.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController")
+
+const usersRoutes = Router();
+
+function myMiddleware(request, response, next){
+  
+}
+
+
+
+
+const usersController = new UsersController();
+
+
+usersRoutes.post("/", usersController.create)
+
+module.exports = usersRoutes;
