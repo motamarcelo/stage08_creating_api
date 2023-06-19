dfe00eea29c76f8b3167d2e426d8986123d04e24
marcelo <marcelomotadev@gmail.com>
2023-06-19 16:35:16 -0300

    Finished adding email and name check

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,19 +4,11 @@ const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router();
 
-// function myMiddleware(request, response, next){
-//   console.log("Passou pelo Middleware");
-//   const isAdmin = request.body.isAdmin;
-
-//   isAdmin? next() : response.json({message: 'user unauthorized'})
-// }
-
-
-
 
 const usersController = new UsersController();
 
 
-usersRoutes.post("/", usersController.create)
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
--- src/server.js
+++ src/server.js
@@ -1,40 +1,33 @@
 require('express-async-errors')
 
-const AppError = require("./utils/AppError");
-const express = require('express');
+const migrationsRun = require('./database/sqlite/migrations')
+const AppError = require('./utils/AppError')
+const express = require('express')
 
-const routes = require("./routes/index.js");
+const routes = require('./routes/index.js')
 
-const app = express();
-app.use(express.json());
+migrationsRun();
 
-app.use(routes);
+const app = express()
+app.use(express.json())
 
-app.use(( error, request, response, next) => {
-  
-  if(error instanceof AppError) {
+app.use(routes)
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
     return response.status(error.statusCode).json({
-      status: "error",
+      status: 'error',
       message: error.message
-    });
-   };
+    })
+  }
 
-   console.error(error);
+  console.error(error)
 
-   return response.status(500).json({
+  return response.status(500).json({
     status: 'error',
     message: 'Internal server error'
-  });
-
-});
+  })
+})
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
-
-
-
-
-
-
-
-
