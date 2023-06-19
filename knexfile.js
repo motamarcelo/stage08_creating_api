48232a788487a01da73ad577d1c91409af2c91f3
marcelo <marcelomotadev@gmail.com>
2023-06-19 19:33:50 -0300

    add db table via migrations

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: oath.resolve(_dirname, "src", "database", "database.db")
+      filename: path.resolve(__dirname, "src", "database", "database.db")
     },
-    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true
   }
 };
